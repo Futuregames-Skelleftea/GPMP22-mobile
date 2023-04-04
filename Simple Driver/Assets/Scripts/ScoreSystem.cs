e2bc47528d116def0cb4de4c0909270663cf39da
Oskar Dufva Sundström <oskar_s_9@hotmail.com>
2023-04-04 16:10:55 +0200

    Simple Driver commit

---
+++ Simple Driver/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    public const string HighScoreKey = "HighScore";
+
+    private float score;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime * scoreMultiplier;
+
+        scoreText.text = Mathf.RoundToInt(score).ToString();
+        
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
+    }
+}
