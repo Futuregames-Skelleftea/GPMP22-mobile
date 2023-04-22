9ca7dd53c88cb754f9570d2ca03effec3a95dddb
ChristofferNL <85027479+ChristofferNL@users.noreply.github.com>
2023-04-22 08:57:57 +0200

    Added spawnerscript

--- Assets/AsteroidSpawner.cs
+++ Assets/AsteroidSpawner.cs
@@ -1,18 +1,35 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private GameObject asteroidPrefabs;
+	[SerializeField] private float secondsBetweenAsteroids;
+	[SerializeField] private Vector2 asteroidInitialForceRange;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public enum ScreenSides { Left, Right, Up, Down };
+
+	private void Start()
+	{
+		StartCoroutine(AsteroidSpawnChain());
+	}
+
+	private void SpawnAsteroid()
+	{
+		asteroidInitialForceRange = new Vector2(0, Random.Range(0, 4));
+		int side = Random.Range(0, 4);
+	}
+
+	private IEnumerator AsteroidSpawnChain()
+	{
+		SpawnAsteroid();
+		float progress = 0;
+		while (progress < secondsBetweenAsteroids)
+		{
+			progress += Time.deltaTime;
+			yield return null;
+		}
+		StartCoroutine(AsteroidSpawnChain());
+	}
 }
